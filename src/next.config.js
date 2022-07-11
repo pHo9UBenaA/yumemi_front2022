fedd6fb1892909e4ab2659151390d70cd48f400e
ix0ApwGlyW <hotya0001@gmail.com>
2022-07-11 22:41:29 +0900

    git init

---
+++ src/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  future: {
+    webpack5: true,
+  },
+}
+
+module.exports = nextConfig
